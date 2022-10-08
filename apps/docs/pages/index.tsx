195fe953bac88fbcd4ce9db48b6da2eae173a6b2
thomasneger <thomas.neger@live.com>
2022-10-08 18:33:42 +0200

    chore: setup NextUI

--- apps/docs/pages/index.tsx
+++ apps/docs/pages/index.tsx
@@ -1,10 +1,10 @@
-import { Button } from "ui";
+import { Button } from "@nextui-org/react";
 
 export default function Docs() {
   return (
     <div>
       <h1>Docs</h1>
-      <Button />
+      <Button>Boop</Button>
     </div>
   );
 }

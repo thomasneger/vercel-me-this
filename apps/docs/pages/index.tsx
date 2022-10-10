1eef7c9e75aea47046fc3b9efc114402ea3c6cb1
thomasneger <thomas.neger@live.com>
2022-10-10 19:40:55 +0200

    fix: dynamically import user progress in home
    Only serve the code when the feature is enabled.

--- apps/docs/pages/index.tsx
+++ apps/docs/pages/index.tsx
@@ -1,11 +1,32 @@
 import { Container, Spacer } from '@nextui-org/react';
-import { UserProgress } from '../components/user-progress';
+import { GetStaticProps } from 'next';
+import dynamic from 'next/dynamic';
+import { Suspense } from 'react';
+import { Features, loadFeatures } from '../lib/features';
 
-export default function Docs() {
+interface Props {
+  features: Features;
+}
+
+const DynamicUserProgress = dynamic(() =>
+  import('../components/user-progress').then((mod) => mod.UserProgress)
+);
+
+export default function Docs({ features }: Props) {
+  const { userProgress } = features;
+
   return (
     <Container>
       <Spacer />
-      <UserProgress />
+      <Suspense>{userProgress && <DynamicUserProgress />}</Suspense>
     </Container>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const features = loadFeatures();
+
+  return {
+    props: { features },
+  };
+};

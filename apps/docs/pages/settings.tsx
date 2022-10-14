381773774b70c96447436db822b93dcafd5d31f6
thomasneger <thomas.neger@live.com>
2022-10-14 13:24:48 +0200

    fix(settings): improve feature toggle row layout

--- apps/docs/pages/settings.tsx
+++ apps/docs/pages/settings.tsx
@@ -1,5 +1,12 @@
 import { useTheme as useNextTheme } from 'next-themes';
-import { Container, Switch, useTheme, Text, Spacer } from '@nextui-org/react';
+import {
+  Container,
+  Switch,
+  useTheme,
+  Text,
+  Spacer,
+  Row,
+} from '@nextui-org/react';
 
 export default function Profile() {
   const { setTheme } = useNextTheme();
@@ -9,18 +16,18 @@ export default function Profile() {
     <Container>
       <Text h1>General</Text>
       <Text h2>Theme</Text>
-      <Text>
-        You can pick your preferred theme. It will be cached for the next time
-        you visit 🎨.
-      </Text>
-
-      <Spacer />
-
-      <Switch
-        checked={isDark}
-        onChange={(e) => setTheme(e.target.checked ? 'dark' : 'light')}
-        title="Toggle theme"
-      />
+      <Row align="center">
+        <Switch
+          checked={isDark}
+          onChange={(e) => setTheme(e.target.checked ? 'dark' : 'light')}
+          title="Toggle theme"
+        />
+        <Spacer x={1} />
+        <Text>
+          You can pick your preferred theme. It will be cached for the next time
+          you visit 🎨.
+        </Text>
+      </Row>
     </Container>
   );
 }
